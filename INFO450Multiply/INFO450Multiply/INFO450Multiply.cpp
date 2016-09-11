3a356dd54d04729dbd3cff2ced9a07b063492395
kbalinang <louieb94@gmail.com>
2016-09-11 15:33:01 -0400

    final commit

--- INFO450Multiply/INFO450Multiply/INFO450Multiply.cpp
+++ INFO450Multiply/INFO450Multiply/INFO450Multiply.cpp
@@ -3,17 +3,29 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main()
+int main(void)
 {
-	int i;
-	std::cout << "Multiplcation Table:" << std::endl;
-	while (i = 1, i < 10, i++)
-	{
-		cout << i << endl;
-	}
-	
-    return 0;
+	cout
+		<< "Project by Kristoffer Louie Balinang" << endl
+		<< " " << endl
+		<< "Multiplication Table:" << endl
+		<< " " << endl
+		<< "   0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10" << endl
+		<< " " << endl;
+  
+     for(int column = 0; column <= 10; column++)
+     {
+          cout << column << "  ";
+			
+			for(int row = 0; row <= 10; row++)
+			 {
+			cout << row * column << '\t';
+			 }
+         
+		cout << endl;
+     }
+     return 0;
 }
-
